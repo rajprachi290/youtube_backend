5f3d5473d2bbe331b9fa8b504fcf391db5d99abd
rajprachi290 <raj.prachi290@gmail.com>
2024-06-02 20:09:28 +0530

    added utils to handle error,response and asynchronous functions

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
- 
+import app from './app.js'
 dotenv.config({
-    path:'./env'
-})
+  path: './.env'
+});
 
 connectDB()
+  .then(()=>{
+    app.on("error",(error)=>{
+        console.log("error",error);
+    })
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.log("mongoDB connection failed!!!"), err;
+  });
+
 // import express from "express";
 // const app = express()(async () => {
 //   try {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+//used to create a wrapper function
+
+//.........used promises
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req,res,next)).catch((error) => next(err));
+  };
+};
+
+
+
+
+
+
+
+
+//.......used try catch .....
+// const asyncHandler=(fn)=> async (req,re,next)=>{//higher order function->accepts a functionas a parameter
+//   try{
+//       await fn(req,res,next)
+//   }
+//   catch(error){
+//     res.status(err.code||500).json.({
+//         success:false,
+//         message: err.message
+//     })
+//   }
+// }
+
+export { asyncHandler };
